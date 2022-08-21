a80fbf1bdf1897586beb963982d2d566249ba4fd
Angelo Aldecoa <angelokn14@gmail.com>
2022-08-21 12:19:53 -0700

    create simple flask server

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello container")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_container():
+    return "Hello container"
+
+@app.route("/html")
+def hello_container_html():
+    return "<h1>Hello container</h1>"
+
+if __name__ == '__main__':
+   app.run('0.0.0.0','8080')
